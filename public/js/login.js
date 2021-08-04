55b1412541d6ff7031de54c863fb5c02b10ccecd
isaac-Jurado <kuasi54@hotmail.com>
2021-08-04 17:23:43 -0500

    inicio del repo

---
+++ public/js/login.js
@@ -0,0 +1,3 @@
+function loginDeUsuario(){
+    return false;
+}
---
+++ vistas/footer.php
@@ -0,0 +1,10 @@
+        <script src="public/jquery/jquery.js"></script>
+        <script src="public/js/boostrap.js"></script>
+        <script src="public/pooper/popper.js"></script>
+        <script src="public/datatable/jquery.dataTables.min.js"></script>
+        <script src="public/datatable/dataTables.bootstrap4.min.js"></script>
+        <script src="public/sweetalert/sweetalert2@11.js"></script>
+        <script src="public/plotly/plotly-2.3.0.min.js"></script>
+    </body>
+
+</html>
